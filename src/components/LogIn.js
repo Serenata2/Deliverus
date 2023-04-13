e139c5495c5d74ccb07875eae90cc68404133b8d
Serenata2 <astg98@gmail.com>
2023-04-13 12:30:47 +0900

    서버와 API 통신을 통해 Login, Logout을 구체화했습니다.

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,66 +1,62 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import {Link} from "react-router-dom";
+import {useState} from "react";
+import {API} from '../config';
 
-const LogIn = ({ handleLogIn }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+const LogIn = ({handleLogIn}) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  const handleIdInput = (e) => {
-    setUsername(e.target.value);
-  };
+    const handleIdInput = (e) => {
+        setUsername(e.target.value);
+    };
 
-  const handlePwInput = (e) => {
-    setPassword(e.target.value);
-  };
+    const handlePwInput = (e) => {
+        setPassword(e.target.value);
+    };
 
-  const getLogInResult = async (data) => {
-    const url = "http://localhost:8080/login";
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify(data),
-    });
-    return response.text();
-  };
+    const getLogInResult = async (data) => {
+        const response = await fetch(`${API.LOGIN}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(data),
+        });
+        return response.json();
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = { userid: username, passwd: password };
-    try {
-      const result = await getLogInResult(data);
-      if (result === "success") {
-        console.log("Log In Success");
-        handleLogIn();
-      } else {
-        console.log("Log In failed from server");
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {userid: username, passwd: password};
+        try {
+            const result = await getLogInResult(data);
+            console.log("Log In Success", result);
+            handleLogIn(result.userId);
+        } catch (err) {
+            console.log("Error in Login : ", err);
+        }
+    };
 
-  return (
-    <>
-      <form onSubmit={handleSubmit} method="post">
-        <label htmlFor="id">ID: </label>
-        <input onChange={handleIdInput} id="id" type="text" value={username} />
-        <br />
-        <label htmlFor="password">PW: </label>
-        <input
-          onChange={handlePwInput}
-          id="password"
-          type="password"
-          value={password}
-        />
-        <br />
-        <button>로그인</button>
-      </form>
-      <Link to="/register">회원가입</Link>
-    </>
-  );
+    return (
+        <>
+            <form onSubmit={handleSubmit} method="post">
+                <label htmlFor="id">ID: </label>
+                <input onChange={handleIdInput} id="id" type="text" value={username}/>
+                <br/>
+                <label htmlFor="password">PW: </label>
+                <input
+                    onChange={handlePwInput}
+                    id="password"
+                    type="password"
+                    value={password}
+                />
+                <br/>
+                <button>로그인</button>
+            </form>
+            <Link to="/register">회원가입</Link>
+        </>
+    );
 };
 
 export default LogIn;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import LogIn from "./LogIn";
 import MainContents from "./MainContents";
+import {useContext} from "react";
+import {UserContext} from "../context";
 
-const Main = ({ isLoggedIn, handleLogIn }) => {
-  if (isLoggedIn) {
-    return <MainContents />;
-  } else {
-    return <LogIn handleLogIn={handleLogIn} />;
-  }
+const Main = ({isLoggedIn, handleLogIn}) => {
+    if (isLoggedIn) {
+        return <MainContents/>;
+    } else {
+        return <LogIn handleLogIn={handleLogIn}/>;
+    }
 };
 
 export default Main;
