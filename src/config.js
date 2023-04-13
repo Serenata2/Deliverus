4a1d68fcae01cde96d7d375e85e5d58867ed04cf
Serenata2 <astg98@gmail.com>
2023-04-13 12:28:27 +0900

    자동 정렬을 이용해서 코드 정렬 및 기타 코드

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 // 환경변수 선언
+// 서버 ip 주소 :121.141.69.189
 
 const BASE_URL = "http://172.20.10.3:8080"
 
 export const API = {
     REGISTER: `${BASE_URL}/api/member/register`,
-    LOGIN : `${BASE_URL}/api/member/login`,
-    RESTAURANT: `${BASE_URL}/api/restaurant`,
-    RESTAURANT_MENU: `${BASE_URL}/api/restaurant/menu`,
-    CHAT : `${BASE_URL}/api/chat`
+    LOGIN: `${BASE_URL}/api/member/login`,
+    LOGOUT: `${BASE_URL}/api/member/logout`,
+    RESTAURANT_CATEGORY: `${BASE_URL}/api/restaurant/category`,
+    RESTAURANT_ALL: `${BASE_URL}/api/restaurant/all`,
 }
