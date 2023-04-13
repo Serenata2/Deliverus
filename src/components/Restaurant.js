3e2701ecb29be477945422b3cfddc15cde24d948
Serenata2 <astg98@gmail.com>
2023-04-13 12:31:19 +0900

    서버로부터 가게 정보를 받아오는 API 테스트 진행 중

---
+++ src/components/Restaurant.js
@@ -0,0 +1,60 @@
+import { API } from '../config';
+import {useContext} from "react";
+import {UserContext} from "../context";
+
+/**
+ * 가게 정보를 받아오는 API를 테스트 하는 공간입니다.
+ * 카테고리별로 가게 정보를 받아오거나, 가게 전체를 다 받아올 수 있습니다.
+ */
+const Restaurant = ({ myHandle }) => {
+    const handleLogOutClicked = useContext(UserContext);
+
+    const restaurantCategoryTest = (event) => {
+        event.preventDefault();
+        const data = {category : "중식", address : "남천동"} // 예시 데이터
+        try {
+            fetch(`${API.RESTAURANT_CATEGORY}?category=${data.category}&address=${data.address}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+            })
+                .then((respones) => respones.json())
+                .then((data) => {
+                    console.log("Respones Data from Restaurant Category API : ", data);
+                });
+        } catch(error) {
+            console.log("Error in Restaurant Category API", error);
+            handleLogOutClicked();
+        }
+    }
+
+    const restaurantAllTest = (event) => {
+        event.preventDefault();
+        try {
+            fetch(`${API.RESTAURANT_ALL}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+            })
+                .then((respones) => respones.json())
+                .then((data) => {
+                    console.log("Respones Data from Restaurant All API : ", data);
+                });
+        } catch(error) {
+            console.log("Error in Restaurant All API", error);
+            handleLogOutClicked();
+        }
+    }
+
+
+    return (
+        <>
+            <p><button onClick={restaurantCategoryTest}>카테고리별 가게 확인</button></p>
+            <p><button onClick={restaurantAllTest}>모든 가게 정보 확인</button></p>
+        </>
+    );
+};
+
+export default Restaurant;
