c1abc8657ad149af71f3f508ae64067e76bda2df
Serenata2 <astg98@gmail.com>
2023-05-04 12:48:32 +0900

    AI 추천 카테고리를 보여주는 창을 구현했습니다.

---
+++ src/components/recommendation/RecommendationList.js
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import LooksOneOutlinedIcon from '@mui/icons-material/LooksOneOutlined';
+import LooksTwoOutlinedIcon from '@mui/icons-material/LooksTwoOutlined';
+import Looks3OutlinedIcon from '@mui/icons-material/Looks3Outlined';
+import Looks4OutlinedIcon from '@mui/icons-material/Looks4Outlined';
+import Looks5OutlinedIcon from '@mui/icons-material/Looks5Outlined';
+import {Box, CardActionArea, Typography} from "@mui/material";
+import Card from "@mui/material/Card";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import {Fragment} from "react";
+import ListSubheader from '@mui/material/ListSubheader';
+
+export default function RecommendationList(props) {
+    const iconList = [<LooksOneOutlinedIcon/>, <LooksTwoOutlinedIcon/>, <Looks3OutlinedIcon/>, <Looks4OutlinedIcon/>, <Looks5OutlinedIcon/>];
+    const isMobile = useMediaQuery("(max-width: 1400px)");
+
+    // 모바일 화면인 경우
+    if(isMobile) {
+        return(
+            <Fragment>
+            <h3>📈 AI가 추천해주는 Top5 음식!</h3>
+            <Box align="center" sx={{display: "flex", alignItem: "row", justifyContent: "space-between", mb: 4}}>
+                    {props.list.map((item, idx) => {
+                        if(idx <=5) {
+                            return (<Card key={idx}>
+                                    <CardActionArea>
+                                        <Typography gutterBottom variant="h6" component="div" sx={{margin:1}}>
+                                            {item}
+                                        </Typography>
+                                    </CardActionArea>
+                                </Card>);
+                        }})}
+            </Box>
+            </Fragment>);
+    }
+    // PC 화면인 경우
+    else {
+        return (
+            <List
+                sx={{ width: "100%", maxWidth: 270,
+                    bgcolor: 'background.paper', borderRadius: '16px', border: 3,
+                    position: 'absolute', top : "300px", left : "80vw"
+                }}
+                aria-label="contacts"
+                subheader={
+                    <ListSubheader>
+                        📈 AI가 추천해주는 Top5
+                    </ListSubheader>
+                }
+            >
+                {props.list.map((item, idx) => {
+                    if(idx <=5){
+                        return (<ListItem key={idx}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    {iconList[idx]}
+                                </ListItemIcon>
+                                <ListItemText primary={item}/>
+                            </ListItemButton>
+                        </ListItem>);}
+                    })}
+            </List>
+        );
+    }
+}
---
+++ src/components/restaurant/RestaurantInfo.js
@@ -0,0 +1,132 @@
+import {Fragment, useContext, useEffect, useState} from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Image from 'mui-image';
+import Button from "@mui/material/Button";
+import Stack from '@mui/material/Stack';
+import RecruitingPartyCard from "./RecruitingPartyCard";
+import MenuCard from "./MenuCard";
+import {API} from "../../utils/config";
+import * as status from "../../utils/status";
+import {UserContext} from "../store/UserContext";
+
+export const RecruitingParty = () => {
+    return (<Fragment>
+        <Typography component="h6" variant="h6" sx={{mb: 1}}>
+            현재 모집 중인 딜리버스
+        </Typography>
+        <RecruitingPartyCard/>
+        <Button
+            fullWidth
+            variant="contained"
+            sx={{mt: 3, mb: 2}}
+            onClick={() => alert("내가 딜리버스 모집하기가 클릭되었습니다.")}
+        >내가 딜리버스 모집하기</Button>
+    </Fragment>);
+}
+
+// 가게 조회 화면 컴포넌트입니다.
+// prop으로 보여주고자 하는 가게 ID을 받습니다.
+const RestaurantInfo = ({restaurantName}) => {
+    const { handleLogOut } = useContext(UserContext);
+    const [restaurant, setRestaurant] = useState({
+        name: "string",
+        address: "string",
+        phoneNumber: "string",
+        category: "string",
+        rating: 0,
+        menu: {
+            menu: [
+                {
+                    "menuName": "string",
+                    "price": 0
+                }
+            ]
+        }
+    });
+
+    // 처음 페이지에 들어갈 때, prop에 있는 가게의 ID를 가지고 서버로부터 가게 정보 받기
+    useEffect(() => {
+        const data = { restaurant_id: 1};
+        fetch(`${API.RESTAURANT_INFORMATION}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(data)
+        })
+            .then((respones) => {
+                status.handleRestaurantResponse(respones.status);
+                return respones.json();
+            })
+            .then((data) => {
+                console.log("Respones Data from Restaurant Info API : ", data);
+                setRestaurant(data);
+            })
+            .catch((error) => {
+                // 로그인 만료 에러인 경우 로그아웃 실행
+                if (error.name === "LoginExpirationError") {
+                    handleLogOut();
+                }
+                // 요청한 것에 대한 데이터가 벗을 때 에러 처리
+                else if(error.name === "NoDataError") {
+                    alert("error.message");
+                }
+                console.log(`${error.name} : ${error.message}`);
+            });
+    }, []);
+
+    // 이미지를 src/images 디렉토리에서 가져옵니다.
+    // 향후 이미지를 카테고리 별로 S3에 저장해서 모듈화 해야겠습니다. ex) require(`../../images/restaurant/${category}/${restaurantName}.png)`
+    const image = require("../../images/chicken/bhc.png");
+
+    console.log("res : ", restaurant);
+    const restaurantDescript = (<Box sx={{
+        my: 2, display: "flex", flexDirection: "column", alignItems: "center", border: 1, borderRadius: '16px', py: 2
+    }}>
+        <Image src={image}
+               height="250px"
+               widht="250px"
+               fit="contain"
+               duration={1000}
+        />
+        <Typography component="h3" variant="h3" sx={{my: 3}}>
+            {restaurant.name}
+        </Typography>
+        <Typography component="h6" variant="h6">
+            가게 설명 텍스트
+        </Typography></Box>);
+
+    // 가게 메뉴 정보 받아오기
+    const MenuList = restaurant.menu.menu;
+    const restaurantMenu = (<Fragment>
+        <Typography component="h6" variant="h6" sx={{mb: 1}}>
+            메뉴
+        </Typography>
+        <Stack spacing={3}>
+            {MenuList.map((item, index) => {
+                return (<MenuCard key={index} menu={item}/>);
+            })}
+        </Stack>
+    </Fragment>);
+
+    return (
+        <>
+            <Box component="main" sx={{
+                my: 8,
+                mx: 'auto',
+                px: 4,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                maxWidth: 'md'
+            }}>
+                {restaurantDescript}
+                <RecruitingParty/>
+                {restaurantMenu}
+            </Box>
+        </>);
+}
+
+export default RestaurantInfo;
