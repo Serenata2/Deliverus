d56e3efc8a9b184a9dc2cf7985832e705f520e45
Serenata2 <astg98@gmail.com>
2023-05-05 11:51:28 +0900

    가게의 이미지들을 추가했습니다.

--- src/components/restaurant/MenuCard.js
+++ src/components/restaurant/MenuCard.js
@@ -5,11 +5,30 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
+import Tooltip from '@mui/material/Tooltip';
+import {useState} from "react";
 
 // 가게의 하나의 Menu 정보를 보여주는 컴포넌트입니다.
 // prop으로 메뉴 이름, 가격을 담은 객체를 받습니다.
 const MenuCard = ({menu}) => {
-    return (<Card variant="outlined" sx={{display: "flex"}}>
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleToggle = () => {
+        const nextState = !open;
+        setOpen(nextState);
+    };
+
+    const menuIntro = menu.menuIntro;
+
+    return (<Card variant="outlined"  sx={{display: "flex"}}>
         <CardContent sx={{my: "auto", px: 0, pl: 1}}>
             <LunchDiningIcon/>
         </CardContent>
@@ -19,12 +38,14 @@ const MenuCard = ({menu}) => {
                     {menu.menuName}
                 </Typography>
                 <Typography fontSize='0.7rem' variant="body2">
-                    가격 : {menu.price}
+                    가격 : {menu.price}원
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small" onClick={() => alert("메뉴에서 자세히 보기가 클릭!")}>
+                <Tooltip open={open} title={menuIntro}>
+                <Button size="small" onClick={handleToggle}>
                     자세히 보기</Button>
+                </Tooltip>
             </CardActions>
         </Box>
     </Card>);
