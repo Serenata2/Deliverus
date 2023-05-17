5619206e014d86c91b7945ad25b17f951f5b4932
Serenata2 <astg98@gmail.com>
2023-05-17 22:27:10 +0900

    Swagger에 따라 참가자 입장에서 메뉴 선택 후 API 전송 구현

--- src/components/partyRoom/partyRoomEnter/PersonalMenuSelecting.js
+++ src/components/partyRoom/partyRoomEnter/PersonalMenuSelecting.js
@@ -2,19 +2,25 @@ import Box from "@mui/material/Box";
 import React, {useContext, useEffect, useState} from "react";
 import MenuSelecting from "../partyRoomCreate/MenuSelecting";
 import Button from "@mui/material/Button";
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import {UserContext} from "../../store/UserContext";
 import * as status from "../../../utils/status";
 import { API } from "../../../utils/config";
 
-// 참가자 입장에서 가게의 메뉴를 선택하는 컴포넌트입니다.
-// 하나의 가게에 대한 메뉴 배열을 prop으로 받습니다.
-function EachMenuSelecting() {
-    const {handleLogOut} = useContext(UserContext);
+// 참가자 입장에서 가게의 메뉴를 선택하는 컴포넌트입니다
+function PersonalMenuSelecting() {
+    const context = useContext(UserContext);
+    const {userState, handleLogOut} = context;
+    const {username} = userState;
+
+    const navigate = useNavigate();
 
     // 가게 ID 정보를 받아오기
     const location = useLocation();
-    const restaurantId = location.state;
+    const restaurantId = location.state.resId;
+    const partyId = location.state.partyId;
+
+    console.log("party id : ", partyId);
 
     // 서버로부터 가게 정보를 받을 변수
     const [restaurant, setRestaurant] = useState({
@@ -68,6 +74,49 @@ function EachMenuSelecting() {
 
     const handleNext = () => {
         alert("파티방 화면으로 이동!");
+
+        // 사용자가 선택한 메뉴에 대한 정보 담기
+        const orderList = [];
+        restaurant.menu.menu.map((item, index) => {
+            if(countList[index] > 0){
+                orderList.push({
+                    name : item.menuName,
+                    price : item.price,
+                    num : countList[index]
+                })
+            }
+        })
+
+        // 최종적으로 서버에게 보낼 데이터 형태
+        const data = {
+            partyId : partyId,
+            nickname : username,
+            order : orderList
+        };
+        fetch(`${API.PARTY_MEMBER}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(data)
+        })
+            .then((respones) => {
+                status.handlePartyResponse(respones.status);
+                return respones.text();
+            })
+            .then((data) => {
+                console.log("Respones Data from Party Member API : ", data);
+                // 파티방 참가 API 호출 후 main 화면으로 이동
+                navigate("/");
+            })
+            .catch((error) => {
+                // 로그인 만료 에러인 경우 로그아웃 실행
+                if (error.name === "LoginExpirationError") {
+                    handleLogOut();
+                }
+                console.log(`${error.name} : ${error.message}`);
+            });
     };
 
     return (<Box sx={{display: "flex", flexDirection: "column", alignItems: "center",
@@ -81,4 +130,4 @@ function EachMenuSelecting() {
     </Box>);
 }
 
-export default EachMenuSelecting;
+export default PersonalMenuSelecting;
