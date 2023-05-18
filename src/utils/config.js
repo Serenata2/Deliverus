12627eef1d87e68d27a93f7c72b27c37bd3d9433
Serenata2 <astg98@gmail.com>
2023-05-19 00:38:39 +0900

    API 수정

--- src/utils/config.js
+++ src/utils/config.js
@@ -9,8 +9,10 @@ export const API = {
     RESTAURANT_LIST: `${BASE_URL}/api/restaurant/list`,
     RESTAURANT_INFORMATION: `${BASE_URL}/api/restaurant/info`,
     RESTAURANT_ALL: `${BASE_URL}/api/restaurant/all`,
-    PARTY: `${BASE_URL}/api/party`, // 파티방 생성 API
-    PARTY_LOCATION: `${BASE_URL}/api/party/location`, // 내 위치 주변에 생성된 파티방 가져오는 API
-    PARTY_VALIDATION: `${BASE_URL}/api/party/validation`,
+    PARTY: `${BASE_URL}/api/party`, // POST : 파티방 생성 API, GET : 파티방 정보 조회 API
+    PARTY_ALL: `${BASE_URL}/api/party/all`, // 내 위치 주변에 생성된 파티방 가져오는 API
+    PARTY_ID: `${BASE_URL}/api/party/id`,
     PARTY_MEMBER : `${BASE_URL}/api/party/member`,
+    PARTY_DELETE : `${BASE_URL}/api/party/member`,
+    PARTY_RESTAURANT : `${BASE_URL}/api/party/restaurant`,
 }
