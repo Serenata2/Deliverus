18d2386dc0e3de9a661c037887df6ee6367b49cc
Serenata2 <astg98@gmail.com>
2023-05-27 01:55:24 +0900

    예외처리 추가

--- src/components/ui/LetterAvatar.js
+++ src/components/ui/LetterAvatar.js
@@ -1,28 +1,24 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import chat from "../myPage/chatting/Chat";
 
 function stringToColor(string) {
     let hash = 0;
     let i;
-    try {
-        /* eslint-disable no-bitwise */
-        for (i = 0; i < string.length; i += 1) {
-            hash = string.charCodeAt(i) + ((hash << 5) - hash);
-        }
 
-        let color = '#';
-
-        for (i = 0; i < 3; i += 1) {
-            const value = (hash >> (i * 8)) & 0xff;
-            color += `00${value.toString(16)}`.slice(-2);
-        }
-        /* eslint-enable no-bitwise */
-
-        return color;
-    } catch (e) {
-        return "#ffffff"
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+        hash = string.charCodeAt(i) + ((hash << 5) - hash);
     }
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) {
+        const value = (hash >> (i * 8)) & 0xff;
+        color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
 }
 
 function stringAvatar(name) {
